eb44673a77e9910c19b90dd50dc6be0a6c450406
makona-OG <makonapharel@gmail.com>
2023-09-12 19:07:50 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar as a message.
- *                                           * Return: Always 0 (Success)
- */                                         int main(void)
-{                                                   int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-        int count, sz;
+ * main - Prints -putchar as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int str[] = {45, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-        sz = sizeof(str) / sizeof(int);             for (count = 0; count < sz; count++)
-        {                                                   _putchar(str[count]);
-        }                                           _putchar('\n');                             return (0);                         }
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
